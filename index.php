bf76b210b1000447b2abad5a5648f4d8b87edf26
UnNoobyEs <unnoobyessanctity@gmail.com>
2022-03-15 04:10:56 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<header>
+    <nav>
+        <div class="container">
+           <div class="navbar-wrap menu-container">
+               <a href="index.php"><img src="img/logo.png" alt="Логотип" class="logo"></a>
+               <div class="menu">
+                   <ul>
+                       <li>
+                           <a href="#">О компании</a>
+                       </li>
+                       <li>
+                           <a href="#">Деятельность</a>
+                       </li>
+                       <li>
+                           <a href="#">Принципы</a>
+                       </li>
+                       <li>
+                           <a href="#">Партнерство</a>
+                       </li>
+                       <li>
+                           <a href="#">Инвестиционная стратегия</a>
+                       </li>
+                       <li>
+                           <a href="#">Инвестиционный профиль</a>
+                       </li>
+                   </ul>
+               </div>
+           </div>
+        </div>
+    </nav>
+</header>
+<section>
+    <h1>ABC</h1>
+</section>
+
+<script src="js/jquery.min.3.5.1.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,19 @@
+//line menu
+$(document).ready(function(){
+    $('.menu li a').click(function(){
+        $('li a').removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
+//scroll menu
+
+window.onscroll = function showHeader() {
+    let header = document.querySelector('header');
+
+    if(window.pageYOffset > 200) {
+        header.classList.add('header_fix');
+    } else {
+        header.classList.remove('header_fix');
+    }
+}
